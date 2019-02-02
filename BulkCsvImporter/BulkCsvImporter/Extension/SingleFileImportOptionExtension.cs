77d4c9bac68b2188cc6eb87c585040c61fd98ee7
孙珽 <sunting@zhongruigroup.com>
2019-02-02 14:31:38 +0800

    Fill the comments.

--- BulkCsvImporter/BulkCsvImporter/Extension/SingleFileImportOptionExtension.cs
+++ BulkCsvImporter/BulkCsvImporter/Extension/SingleFileImportOptionExtension.cs
@@ -9,12 +9,25 @@ namespace BulkCsvImporter.Extension
 {
     public static class SingleFileImportOptionExtension
     {
+        /// <summary>
+        /// Build a local single file source option for import.
+        /// </summary>
+        /// <param name="option">A single file option</param>
+        /// <param name="filePath">The file path of local file</param>
+        /// <returns>SingleFileImportOption</returns>
         public static SingleFileImportOption BuildLocalFileSource(this SingleFileImportOption option, string filePath)
         {
             option.FileSourceOption = new LocalFileSourceOption(filePath);
             return option;
         }
 
+        /// <summary>
+        /// Build data base connect for a local single file import.
+        /// </summary>
+        /// <param name="option">A single file option</param>
+        /// <param name="databaseType">The database type</param>
+        /// <param name="connectionString">The connetion string</param>
+        /// <returns>SingleFileImportOption</returns>
         public static SingleFileImportOption BuildDatabaseConnect(this SingleFileImportOption option, DatabaseType databaseType, string connectionString)
         {
             option.DatabaseConnectOption.DatabaseType = databaseType;
@@ -22,6 +35,15 @@ namespace BulkCsvImporter.Extension
             return option;
         }
 
+        /// <summary>
+        /// Build import target for a local single file import.
+        /// </summary>
+        /// <param name="option">A single file option</param>
+        /// <param name="needUpdate">If records with same business PK, set to true; if only need insert, set to false.</param>
+        /// <param name="targetTableName">The table name the csv should import to</param>
+        /// <param name="columns">The table columns</param>
+        /// <param name="keyColumns">The business PK columns</param>
+        /// <returns></returns>
         public static SingleFileImportOption BuildImportTarget(this SingleFileImportOption option, bool needUpdate, string targetTableName, List<string> columns, List<string> keyColumns)
         {
             option.ImportTargetOption.NeedUpdate = needUpdate;
